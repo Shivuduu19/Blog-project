81173d1c913a3352a381c777709b1c5a92866a82
shiva kumar <98257507+Shivuduu19@users.noreply.github.com.com>
2024-04-06 12:08:15 +0530

    edited conf

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -4,7 +4,6 @@ const conf = {
     appwriteDataBaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
     appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
     appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
-    appwriteLikesCollectionId: String(import.meta.env.VITE_APPWRITE_LIKES_COLLECTION_ID),
     TinyMce: String(import.meta.env.VITE_TINYMCE_KEY),
 };
 export default conf;
